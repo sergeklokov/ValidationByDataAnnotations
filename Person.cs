37846e87ff6415cf9d64cbcece1f245ca99e695f
sergeklokov <serge.klokov@gmail.com>
2015-06-03 15:23:02 -0700

    Init
    Demo of using data annotation to validate fields of the object

---
+++ Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations; // please add .dll for it
+
+namespace ValidationByDataAnnotations
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "LastName is required")]
+        public string LastName { get; set; }
+
+
+        private int m_Age;
+        [Range(1, 120, ErrorMessage="Age is wrong")]
+        public int Age
+        {
+            get { return m_Age; }
+            set { m_Age = value; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ValidationByDataAnnotations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var person = new Person { Name = "Serge", LastName = "Klokov", Age = 55 };
+            var validationContext = new ValidationContext(person, serviceProvider: null, items: null);
+            var validationResults = new List<ValidationResult>();
+            var isValid = Validator.TryValidateObject(person, validationContext, validationResults, true);
+
+
+            if (isValid)
+                Console.WriteLine("{0} is valid", person.Name);
+            else
+                foreach (var validationResult in validationResults)
+                    Console.WriteLine("Validation error: {0}", validationResult);
+
+            // **************************************** 
+
+            var person2 = new Person { Name = "Bad", Age = 300 };
+            validationContext = new ValidationContext(person2, serviceProvider: null, items: null);
+            validationResults = new List<ValidationResult>();
+            isValid = Validator.TryValidateObject(person2, validationContext, validationResults, true);
+
+            if (isValid)
+                Console.WriteLine("{0} is valid", person2.Name);
+            else
+                foreach (var validationResult in validationResults)
+                    Console.WriteLine("Validation error: {0}", validationResult);
+
+
+            Console.ReadKey();
+        }
+    }
+}
